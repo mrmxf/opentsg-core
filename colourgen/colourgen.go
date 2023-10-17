8409970e36176b4ece75825e9c9f13fb233034c7
Tristan Larke <tristan@mrmxf.com>
2023-10-17 14:59:32 +0000

    updated returned colour

--- colourgen/colourgen.go
+++ colourgen/colourgen.go
@@ -46,7 +46,7 @@ func AssignRGBValues(colour string, rgb, maxBlack, maxWhite int) ([3]int, error)
 // Acceptable formats are #rgb, #rgba, #rrggbb, ##rrggbbaa, rgb(r,g,b), rgba(r,g,b,a), rgb12(r,g,b) and rgba12(r,g,b,a)
 //
 // The resulting value is either color.NRGBA or color.NRBGA64, 12 bit RGB values are represented in 16 bit NRGBA64.
-func HexToColour(colorCode string, space colour.ColorSpace) color.Color {
+func HexToColour(colorCode string, space colour.ColorSpace) *colour.CNRGBA64 {
 	var base *colour.CNRGBA64
 	regRRGGBB := regexp.MustCompile(`^#[A-Fa-f0-9]{6}$`)
 	regRGB := regexp.MustCompile(`^#[A-Fa-f0-9]{3}$`)
